dc7aeba97857d0384a4f8439a05c53a92e01295a
Camila Bringel <113627499+CamilaBringell@users.noreply.github.com>
2023-04-23 14:42:40 -0300

    Create Questão1.java

---
+++ Ativ1/Questão1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.questao_1;
+
+/**
+ *
+ * @author Camila Bringel
+ */
+public class Questao_1 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 51; i++){
+             System.out.println( + i);
+                }
+    }
+}
+    
+
