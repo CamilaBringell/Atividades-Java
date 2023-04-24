1b217de71348070a3ff31f4ac41f97b05cc3c537
Camila Bringel <113627499+CamilaBringell@users.noreply.github.com>
2023-04-24 20:16:48 -0300

    Create questão7.java

---
+++ Ativ1/questão7.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.questao_7;
+
+/**
+ *
+ * @author Camila Bringel
+ */
+public class Questao_7 {
+
+    public static void main(String[] args) {
+        int cont = 1;
+        while (cont <= 11){
+            System.out.println("Hello World!");
+            cont++;
+            if (cont == 12){
+                 break;
+            }
+           
+        }
+        
+    }
+}
