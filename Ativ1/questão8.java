bc97484aa687b9c985492c74cc1c0ed7696c238d
Camila Bringel <113627499+CamilaBringell@users.noreply.github.com>
2023-04-24 20:43:15 -0300

    Create questão8.java

---
+++ Ativ1/questão8.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.questao8;
+
+/**
+ *
+ * @author Camila Bringel
+ */
+public class Questao8 {
+
+    public static void main(String[] args) {
+        int cont = 1;
+        while (cont <= 50){
+            System.out.println(cont);
+            cont++;
+        }
+    }
+}
