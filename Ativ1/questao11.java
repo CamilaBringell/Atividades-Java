f534a3288d7911a22a1fd3eab3030f3888b1b42a
Camila Bringel <113627499+CamilaBringell@users.noreply.github.com>
2023-04-25 21:44:36 -0300

    Create questao11.java

---
+++ Ativ1/questao11.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.questao11;
+
+/**
+ *
+ * @author Camila Bringel
+ */
+import java.util.Scanner;
+public class Questao11 {
+
+    public static void main(String[] args) {
+        int num;
+        Scanner ler = new Scanner (System.in);
+            System.out.println("Informe um numero de 12 a 20:");
+            num = ler.nextInt();
+        while (num < 12 || num > 20){
+            System.out.println("entrada inválida");
+            break;
+        }
+        if(num >=12 && num <=20){
+            System.out.println(num);
+        }
+    }
+}
