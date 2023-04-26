accc3b8092ed501071f92fdd96637c99c03012a6
Camila Bringel <113627499+CamilaBringell@users.noreply.github.com>
2023-04-25 21:17:06 -0300

    Create questao10.java

---
+++ Ativ1/questao10.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.questao10;
+
+/**
+ *
+ * @author Camila Bringel
+ */
+import java.util.Scanner;
+public class Questao10 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+        int i = 1; 
+        while (i <=10){
+            System.out.println("2 x "+i+" = "+2*i);
+            i++;
+        }
+       
+    }
+}
